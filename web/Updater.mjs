25beba00a7f6e854c6692fd5f3471da1d75c6705
Alexander Fritsch <35202909+loudar@users.noreply.github.com>
2023-11-21 13:04:50 +0100

    add interval for update, so you just have to wait 8)

---
+++ web/Updater.mjs
@@ -0,0 +1,105 @@
+import {Api} from "./Api.mjs";
+import {DataSet} from "vis-data";
+import {Network} from "vis-network";
+
+export class Updater {
+    static async update() {
+        const links = await Api.getLinks();
+        const graph = document.getElementById('graph');
+
+        const nodesArray = [];
+        const edgesArray = [];
+
+        let idCounter = 1;
+        let urlToIdMap = {};
+        let urlFrequencyMap = {};
+
+        const linkKeys = Object.entries(links);
+        for (const [url] of linkKeys) {
+            urlFrequencyMap[url] = links[url] ? links[url].length : 0;
+            urlToIdMap[url] = idCounter++;
+            for (const link of links[url]) {
+                if (!urlToIdMap[link]) {
+                    urlToIdMap[link] = idCounter++;
+                }
+            }
+        }
+
+        let maxFrequency = Math.max(...Object.values(urlFrequencyMap));
+
+        function colorFromFrequency(frequency) {
+            const hue = Math.floor(120 * frequency / maxFrequency);
+            return `hsl(${hue}, 100%, 50%)`;
+        }
+
+        for (const [url, id] of Object.entries(urlToIdMap)) {
+            let size = links[url] ? links[url].length : 0;
+            const relativeSize = size / maxFrequency;
+            const linkHasLinks = links[url] && links[url].length > 0;
+
+            if (!linkHasLinks) {
+                continue;
+            }
+
+            nodesArray.push({
+                id,
+                label: url,
+                shape: "dot",
+                size: 10 + (relativeSize * 50),
+                color: colorFromFrequency(size),
+            });
+        }
+
+        for (const [url, linkList] of linkKeys) {
+            let fromId = urlToIdMap[url];
+            for (const link of linkList) {
+                const linkHasLinks = links[link] && links[link].length > 0;
+                if (!linkHasLinks) {
+                    continue;
+                }
+                edgesArray.push({from: fromId, to: urlToIdMap[link]});
+            }
+        }
+
+        const nodes = new DataSet(nodesArray);
+        const edges = new DataSet(edgesArray);
+
+        let data = {
+            nodes: nodes,
+            edges: edges
+        };
+
+        const options = {
+            physics: {
+                stabilization: false,
+                barnesHut: {
+                    gravitationalConstant: -20000,
+                    springConstant: 0.01,
+                    springLength: 200
+                }
+            },
+            layout: {
+                improvedLayout: false,
+            },
+            edges: {
+                arrows: {
+                    to: {enabled: true, scaleFactor: 1, type: 'arrow'}
+                }
+            },
+            nodes: {
+                scaling: {
+                    label: {
+                        enabled: false,
+                        min: 8,
+                        max: 30
+                    }
+                }
+            },
+            interaction: {
+                hover: true
+            }
+        };
+
+        const network = new Network(graph, data, options);
+    }
+}
--- web/index.mjs
+++ web/index.mjs
@@ -1,101 +1,14 @@
-import {Api} from "./Api.mjs";
-import {DataSet} from "vis-data";
-import {Network} from "vis-network";
+import {Updater} from "./Updater.mjs";
 
-const links = await Api.getLinks();
-const graph = document.getElementById('graph');
+await Updater.update();
 
-const nodesArray = [];
-const edgesArray = [];
-
-let idCounter = 1;
-let urlToIdMap = {};
-let urlFrequencyMap = {};
-
-const linkKeys = Object.entries(links);
-for (const [url] of linkKeys) {
-    urlFrequencyMap[url] = links[url] ? links[url].length : 0;
-    urlToIdMap[url] = idCounter++;
-    for (const link of links[url]) {
-        if (!urlToIdMap[link]) {
-            urlToIdMap[link] = idCounter++;
-        }
+const intervalSeconds = 30;
+let locked = false;
+setInterval(async () => {
+    if (locked) {
+        return;
     }
-}
-
-let maxFrequency = Math.max(...Object.values(urlFrequencyMap));
-
-function colorFromFrequency(frequency) {
-    const hue = Math.floor(120 * frequency / maxFrequency);
-    return `hsl(${hue}, 100%, 50%)`;
-}
-
-for (const [url, id] of Object.entries(urlToIdMap)) {
-    let size = links[url] ? links[url].length : 0;
-    const relativeSize = size / maxFrequency;
-    const linkHasLinks = links[url] && links[url].length > 0;
-
-    if (!linkHasLinks) {
-        continue;
-    }
-
-    nodesArray.push({
-        id,
-        label: url,
-        shape: "dot",
-        size: 10 + (relativeSize * 50),
-        color: colorFromFrequency(size),
-    });
-}
-
-for (const [url, linkList] of linkKeys) {
-    let fromId = urlToIdMap[url];
-    for (const link of linkList) {
-        const linkHasLinks = links[link] && links[link].length > 0;
-        if (!linkHasLinks) {
-            continue;
-        }
-        edgesArray.push({from: fromId, to: urlToIdMap[link]});
-    }
-}
-
-const nodes = new DataSet(nodesArray);
-const edges = new DataSet(edgesArray);
-
-let data = {
-    nodes: nodes,
-    edges: edges
-};
-
-const options = {
-    physics: {
-        stabilization: false,
-        barnesHut: {
-            gravitationalConstant: -20000,
-            springConstant: 0.01,
-            springLength: 200
-        }
-    },
-    layout: {
-        improvedLayout: false,
-    },
-    edges: {
-        arrows: {
-            to: {enabled: true, scaleFactor: 1, type: 'arrow'}
-        }
-    },
-    nodes: {
-        scaling: {
-            label: {
-                enabled: false,
-                min: 8,
-                max: 30
-            }
-        }
-    },
-    interaction: {
-        hover: true
-    }
-};
-
-const network = new Network(graph, data, options);
+    locked = true;
+    await Updater.update();
+    locked = false;
+}, intervalSeconds * 1000);
