557a5ca9f84ed096dd2a2ea7894e1a43374b5b8c
Alexander Fritsch <35202909+loudar@users.noreply.github.com>
2023-11-21 13:41:46 +0100

    only update every 10 mins or on r press

--- web/index.mjs
+++ web/index.mjs
@@ -2,7 +2,7 @@ import {Updater} from "./Updater.mjs";
 
 await Updater.update();
 
-const intervalSeconds = 60;
+const intervalSeconds = 60 * 10;
 let locked = false;
 setInterval(async () => {
     if (locked) {
@@ -12,3 +12,11 @@ setInterval(async () => {
     await Updater.update();
     locked = false;
 }, intervalSeconds * 1000);
+
+document.addEventListener("keyup", async (event) => {
+    if (event.key === "r" && !locked) {
+        locked = true;
+        await Updater.update();
+        locked = false;
+    }
+})
