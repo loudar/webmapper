c349dc8b411fa7b4c889635c88c5c113d2768636
alexa <alex@targoninc.com>
2023-11-22 21:23:32 +0100

    try puppeteer cluster, but it doesnt fkin work

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,9 @@
 import path from "path";
 import {spawn} from "child_process";
 import dotenv from "dotenv";
+import { createRequire } from 'module';
 
+const require = createRequire(import.meta.url);
 dotenv.config();
 const dirname = path.resolve();
 
@@ -13,6 +15,30 @@ export default {
     output: {
         filename: "index.mjs",
         path: path.resolve(dirname, "dist"),
+    },
+    resolve: {
+        fallback: {
+            "fs": false,
+            "readline": false,
+            "net": false,
+            "tls": false,
+            "dns": false,
+            "module": false,
+            "typescript": false,
+            "util": require.resolve("util/"),
+            "path": require.resolve("path-browserify"),
+            "assert": require.resolve("assert/"),
+            "os": require.resolve("os-browserify"),
+            "stream": require.resolve("stream-browserify"),
+            "url": require.resolve("url/"),
+            "process": require.resolve("process/browser"),
+            "http": require.resolve("stream-http"),
+            "https": require.resolve("https-browserify"),
+            "zlib": require.resolve("browserify-zlib"),
+            "crypto": require.resolve("crypto-browserify"),
+            "constants": require.resolve("constants-browserify"),
+            "child_process": import("child_process")
+        }
     },
     devServer: {
         static: {
@@ -32,7 +58,7 @@ export default {
     },
 };
 
-const child = spawn('node ./server.mjs', [
+const child = spawn('node --experimental-modules ./server.mjs', [
     `MYSQL_USER=${process.env.MYSQL_USER}`,
     `MYSQL_PASSWORD=${process.env.MYSQL_PASSWORD}`,
 ], {shell: true});
