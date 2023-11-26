aff5b70fb7fee0f1583bfe3d9babab6e43660383
alexa <alex@targoninc.com>
2023-11-26 10:09:55 +0100

    improve config

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,6 @@
 import path from "path";
-import {spawn} from "child_process";
 import dotenv from "dotenv";
 import HtmlWebpackPlugin from 'html-webpack-plugin';
-import open from 'open';
 
 dotenv.config();
 const dirname = path.resolve();
@@ -30,17 +28,3 @@ export default {
         })
     ]
 };
-
-const child = spawn('node ./server.mjs', [
-    `MYSQL_USER=${process.env.MYSQL_USER}`,
-    `MYSQL_PASSWORD=${process.env.MYSQL_PASSWORD}`,
-    `MYSQL_URL=${process.env.MYSQL_URL}`,
-], {shell: true});
-child.stdout.on('data', function (data) {
-    process.stdout.write(data);
-});
-child.stderr.on('data', function (data) {
-    process.stderr.write(data);
-});
-
-open('http://localhost:3000/search.html')
